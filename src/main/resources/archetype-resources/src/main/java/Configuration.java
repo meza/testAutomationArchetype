c45376294c6c2f47623a30034a179e6665f77789
meza <meza@meza.hu>
2014-03-05 14:41:15 +0000

    initialCommit

---
+++ src/main/resources/archetype-resources/src/main/java/Configuration.java
@@ -0,0 +1,21 @@
+package ${package};
+
+import hu.meza.tools.config.Config;
+import hu.meza.tools.config.Loaders.SystemPropertiesConfiguration;
+import hu.meza.tools.config.Required;
+
+import java.io.File;
+
+public class Configuration {
+
+	private Config config = new Config();
+
+	public Configuration() {
+		config.addHighOrder(new SystemPropertiesConfiguration());
+
+		String configFilename = config.get("config", "config.properties");
+		File file = new File(configFilename);
+		config.add(new Required(file));
+	}
+
+}
---
+++ src/main/resources/archetype-resources/src/test/java/RunTest.java
@@ -0,0 +1,10 @@
+package ${package};
+
+import cucumber.api.SnippetType;
+import cucumber.api.junit.Cucumber;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@Cucumber.Options(tags = "~@ignore", snippets = SnippetType.CAMELCASE)
+public class RunTest {
+}
